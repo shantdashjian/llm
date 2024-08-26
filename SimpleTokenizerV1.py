c4eb18bb1234444604a5ac7785f18a95f37295ed
Shant Dashjian <shantgdashjian@gmail.com>
2024-08-26 17:17:49 +0400

    Add text, preprocessing, and tokenizer

---
+++ SimpleTokenizerV1.py
@@ -0,0 +1,16 @@
+import re
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.token_to_id = vocab
+        self.id_to_token = {i:s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.token_to_id[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.id_to_token[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
---
+++ preprocessing.py
@@ -0,0 +1,18 @@
+import re
+from SimpleTokenizerV1 import SimpleTokenizerV1
+
+with open('the-verdict.txt', 'r', encoding='utf-8') as f:
+    raw_text = f.read()
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item for item in preprocessed if item.strip()]
+
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+
+vocab = {token:integer for integer, token in enumerate(all_words)}
+
+tokenizer = SimpleTokenizerV1(vocab)
+text = """"It's the last he painted, you know,", Mrs. Gisburn said with pardonable pride."""
+ids = tokenizer.encode(text)
+print(ids)
