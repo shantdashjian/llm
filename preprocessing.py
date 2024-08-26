ffa87965e4a388b92fc83d5114480a5eef32f9e6
Shant Dashjian <shantgdashjian@gmail.com>
2024-08-26 17:22:35 +0400

    Update example and gitignore

--- preprocessing.py
+++ preprocessing.py
@@ -16,3 +16,6 @@ tokenizer = SimpleTokenizerV1(vocab)
 text = """"It's the last he painted, you know,", Mrs. Gisburn said with pardonable pride."""
 ids = tokenizer.encode(text)
 print(ids)
+
+text = tokenizer.decode(ids)
+print(text)
