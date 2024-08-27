806e954435d104f7b320f81a8f11ac88ec3d4484
Shant Dashjian <shantgdashjian@gmail.com>
2024-08-27 14:10:57 +0400

    Add GPT dataset class and and an experiment file

--- preprocessing.py
+++ preprocessing.py
@@ -1,18 +1,18 @@
-from importlib.metadata import version
 import tiktoken
+with open('the-verdict.txt', 'r', encoding='utf-8') as f:
+    raw_text = f.read()
 
 tokenizer = tiktoken.get_encoding("gpt2")
 
-text = "Hello, do you like tea? <|endoftext|> In the sunlit terraces of someunknownPlace."
-ids = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-print(ids)
+enc_text = tokenizer.encode(raw_text)
 
-strings = tokenizer.decode(ids)
-print(strings)
+print(len(enc_text))
 
-text = "Akwirw ier"
-ids = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-print(ids)
+enc_sample = enc_text[50:]
+print(enc_sample)
 
-strings = tokenizer.decode(ids)
-print(strings)
+context_size = 4
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
